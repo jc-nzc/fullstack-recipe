344f8dafe7d530c01222b935cedd5b2e8a827f1b
Zero <node.zerocool@gmail.com>
2021-04-24 00:18:36 -0500

    const { createProxyMiddleware } link

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,4 +1,5 @@
 // const proxy = require('http-proxy-middleware');
+// solution: https://github.com/facebook/create-react-app/issues/8550
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(createProxyMiddleware('/api',
