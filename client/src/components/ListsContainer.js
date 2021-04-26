dfdd3ed3a1cea3e74d8720971da020a30d438e70
Zero <node.zerocool@gmail.com>
2021-04-25 23:28:43 -0500

    comp iterate list

--- client/src/components/ListsContainer.js
+++ client/src/components/ListsContainer.js
@@ -13,7 +13,40 @@
 //
 // export default ListsContainer;
 
+// initial state is above
+// Working State Below
 
+// import React, { Component } from 'react';
+// import axios from 'axios';
+// class ListsContainer extends Component {
+//     constructor(props){
+//         super(props)
+//         this.state = {
+//             lists: []
+//         }
+//     }
+//     componentDidMount() {
+//         axios.get('api/v1/lists.json')
+//         .then(response => {
+//             console.log(response)
+//             this.setState({
+//                 lists: response.data
+//             })
+//         })
+//         .catch(error => console.log(error))
+//     }
+//     render() {
+//         return (
+//             <div className="Lists-container">
+//                 Lists
+//             </div>
+//         )
+//     }
+// }
+// export default ListsContainer;
+
+
+// Latest iteration
 import React, { Component } from 'react';
 import axios from 'axios';
 class ListsContainer extends Component {
@@ -35,8 +68,15 @@ class ListsContainer extends Component {
     }
     render() {
         return (
-            <div className="Lists-container">
-                Lists
+            <div className="lists-container">
+                {this.state.lists.map( list => {
+                    return (
+                        <div className="single-list" key={list.id}>
+                            <h4>{list.title}</h4>
+                            <p>{list.excerpt}</p>
+                        </div>
+                    )
+                })}
             </div>
         )
     }
